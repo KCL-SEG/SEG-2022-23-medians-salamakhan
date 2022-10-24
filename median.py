196530d006c316ad597dd9c39e89114944393a8d
salamakhan <salama.khan@kcl.ac.uk>
2022-10-24 15:36:00 +0100

    2nd solution

--- median.py
+++ median.py
@@ -9,11 +9,12 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-if len(numbers) % 2 == 1:
-    median = numbers[int((len(numbers))/2)]
+sort_numbers = sorted(numbers)
+if len(sort_numbers) % 2 == 1:
+    median = sort_numbers[int((len(numbers))/2)]
     print(median)
 else:
-    middlevalue1 = numbers[int((len(numbers))/2)]
-    middlevalue2 = numbers[int((len(numbers))/2)-1]
+    middlevalue1 = sort_numbers[int((len(numbers))/2)]
+    middlevalue2 = sort_numbers[int((len(numbers))/2)-1]
     median = (middlevalue1 + middlevalue2)/2
     print(median)
